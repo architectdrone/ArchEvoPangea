421d60d2cc29952ec61b506772055bf7fc94be2f
Owen Mellema <owenmllm@gmail.com>
2020-11-27 13:15:42 -0600

    Add register names

--- webserver/src/main/java/org/architectdrone/archevo/pangea/webserver/model/RegisterModel.java
+++ webserver/src/main/java/org/architectdrone/archevo/pangea/webserver/model/RegisterModel.java
@@ -1,14 +1,16 @@
 package org.architectdrone.archevo.pangea.webserver.model;
 
+import org.architectdrone.archevo.pangea.implementation.isa.asia.ASIARegister;
 import org.architectdrone.archevo.pangea.implementation.universe.UniverseSettings;
 
 public class RegisterModel {
     public Integer registerValue;
     public Integer registerNumber;
-
+    public String registerName;
     public RegisterModel(Integer registerNumber, Integer registerValue)
     {
         this.registerNumber = registerNumber;
         this.registerValue = registerValue;
+        this.registerName = ASIARegister.fromBinary(registerNumber).toString();
     }
 }
