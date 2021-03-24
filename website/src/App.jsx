32dd16d7303ebe74e8aa03a7ab0578603a383416
Owen Mellema <owenmllm@gmail.com>
2021-03-23 22:00:33 -0500

    Use API wrapper

--- website/src/App.jsx
+++ website/src/App.jsx
@@ -1,248 +1,29 @@
-import {React} from 'react';
+import {React, useState, useEffect} from 'react';
 import './App.css';
 // eslint-disable-next-line max-len
 import CellGridAndInformationDisplay from './components/CellGridAndInformationDisplay/CellGridAndInformationDisplay';
 import WorldController from './components/WorldController/WorldController';
-
-const program = [
-  {
-    instruction: 0,
-    translatedInstruction: 'DO NOTHING',
-  },
-  {
-    instruction: 0,
-    translatedInstruction: 'DO NOTHING',
-  },
-  {
-    instruction: 42,
-    translatedInstruction: 'Boogaloo',
-  },
-  {
-    instruction: 0,
-    translatedInstruction: 'DO NOTHING',
-  },
-  {
-    instruction: 0,
-    translatedInstruction: 'DO NOTHING',
-  },
-  {
-    instruction: 0,
-    translatedInstruction: 'DO NOTHING',
-  },
-  {
-    instruction: 0,
-    translatedInstruction: 'DO NOTHING',
-  },
-  {
-    instruction: 0,
-    translatedInstruction: 'DO NOTHING',
-  },
-  {
-    instruction: 0,
-    translatedInstruction: 'DO NOTHING',
-  },
-  {
-    instruction: 0,
-    translatedInstruction: 'DO NOTHING',
-  },
-  {
-    instruction: 0,
-    translatedInstruction: 'DO NOTHING',
-  },
-  {
-    instruction: 0,
-    translatedInstruction: 'DO NOTHING',
-  },
-  {
-    instruction: 0,
-    translatedInstruction: 'DO NOTHING',
-  },
-  {
-    instruction: 0,
-    translatedInstruction: 'DO NOTHING',
-  },
-  {
-    instruction: 0,
-    translatedInstruction: 'DO NOTHING',
-  },
-  {
-    instruction: 0,
-    translatedInstruction: 'DO NOTHING',
-  },
-  {
-    instruction: 0,
-    translatedInstruction: 'DO NOTHING',
-  },
-  {
-    instruction: 0,
-    translatedInstruction: 'DO NOTHING',
-  },
-  {
-    instruction: 0,
-    translatedInstruction: 'DO NOTHING',
-  },
-  {
-    instruction: 0,
-    translatedInstruction: 'DO NOTHING',
-  },
-  {
-    instruction: 0,
-    translatedInstruction: 'DO NOTHING',
-  },
-  {
-    instruction: 0,
-    translatedInstruction: 'DO NOTHING',
-  },
-  {
-    instruction: 0,
-    translatedInstruction: 'DO NOTHING',
-  },
-  {
-    instruction: 0,
-    translatedInstruction: 'DO NOTHING',
-  },
-  {
-    instruction: 0,
-    translatedInstruction: 'DO NOTHING',
-  },
-  {
-    instruction: 0,
-    translatedInstruction: 'DO NOTHING',
-  },
-  {
-    instruction: 0,
-    translatedInstruction: 'DO NOTHING',
-  },
-  {
-    instruction: 0,
-    translatedInstruction: 'DO NOTHING',
-  },
-  {
-    instruction: 0,
-    translatedInstruction: 'DO NOTHING',
-  },
-  {
-    instruction: 0,
-    translatedInstruction: 'DO NOTHING',
-  },
-  {
-    instruction: 0,
-    translatedInstruction: 'DO NOTHING',
-  },
-  {
-    instruction: 0,
-    translatedInstruction: 'DO NOTHING',
-  },
-];
-
-const registers = [
-  {
-    registerName: 'ENERGY',
-    registerValue: 0,
-  },
-  {
-    registerName: 'LOGO',
-    registerValue: 1,
-  },
-  {
-    registerName: 'GUESS',
-    registerValue: 2,
-  },
-  {
-    registerName: 'REG_A',
-    registerValue: 3,
-  },
-  {
-    registerName: 'REG_B',
-    registerValue: 31,
-  },
-  {
-    registerName: 'REG_C',
-    registerValue: 5,
-  },
-  {
-    registerName: 'REG_D',
-    registerValue: 6,
-  },
-  {
-    registerName: 'IPLOC',
-    registerValue: 7,
-  },
-  {
-    registerName: 'I_ENERGY',
-    registerValue: 8,
-  },
-  {
-    registerName: 'I_LOGO',
-    registerValue: 9,
-  },
-  {
-    registerName: 'I_GUESS',
-    registerValue: 10,
-  },
-  {
-    registerName: 'I_REG_A',
-    registerValue: 11,
-  },
-  {
-    registerName: 'I_REG_B',
-    registerValue: 12,
-  },
-  {
-    registerName: 'I_REG_C',
-    registerValue: 13,
-  },
-  {
-    registerName: 'I_REG_D',
-    registerValue: 14,
-  },
-  {
-    registerName: 'I_IPLOC',
-    registerValue: 15,
-  },
-];
-
-const cell = {
-  x: 5,
-  y: 5,
-  instructionPointer: 4,
-  id: 69420,
-  age: 30,
-  lineage: 3,
-  virility: 5,
-  registers: registers,
-  genome: program,
-};
-
-const cell2 = {
-  x: 1,
-  y: 2,
-  instructionPointer: 4,
-  id: 777,
-  age: 30,
-  lineage: 3,
-  virility: 5,
-  registers: registers,
-  genome: program,
-};
-
-/** Lol */
-function printDude() {
-  console.log('Dude');
-}
+import './util/archevoApiWrapper';
+import {getInformation, getState} from './util/archevoApiWrapper';
 
 /**
  * The Root App.
  */
 function App() {
+  const [cells, setCells] = useState([]);
+  useEffect(() => getState()
+    .then((state) => setCells(state.cells))
+    .then(console.log('Foo'))
+      , []);
+  getState().then((state) => setCells(state.cells));
   return (
     <>
       <WorldController
-        startHandler={printDude}
-        stepHandler={printDude}
-        stopHandler={printDude}
+        startHandler={getInformation}
+        stepHandler={getState}
+        stopHandler={getState}
         currentServerState="LOL"/>
-      <CellGridAndInformationDisplay cells={[cell, cell2]}/>
+      <CellGridAndInformationDisplay cells={cells}/>
     </>
   );
 }
